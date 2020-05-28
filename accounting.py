99fa3301c862a9aef1f22aac47eefc5ccbd00971
nicholaspoon03 <npoon2003@gmail.com>
2020-05-27 23:04:09 -0400

    input complete

--- accounting.py
+++ accounting.py
@@ -19,6 +19,14 @@ liability_name = []
 liability_value = []
 liability_cr_dr = []
 
+capital_name = []
+capital_value = []
+capital_cr_dr = []
+
+drawing_name = []
+drawing_value = []
+drawing_cr_dr = []
+
 revenue_name = []
 revenue_value = []
 revenue_cr_dr = []
@@ -98,7 +106,8 @@ def balance_sheet():
 def entry():
     global entry_complete, asset_name, asset_value, asset_cr_dr, liability_name, liability_value
     global liability_cr_dr, revenue_name, revenue_value, revenue_cr_dr, expense_name, expense_value
-    global expense_cr_dr
+    global expense_cr_dr, capital_name, capital_value, capital_cr_dr, drawing_name, drawing_value
+    global drawing_cr_dr
     home = True
     assets = False
     liabilities = False
@@ -106,16 +115,21 @@ def entry():
     drawings = False
     revenue = False
     expenses = False
+    error = False
     print("Welcome to Accounting Helper\n")
     print("This app allows you to make a chart of accounts, trial balance, income statement, and balance sheet with a\nsimple input of your assets, liabilities, and owner's equity.\n")
     print("First, type 'assets', 'liabilities', 'capital', 'drawings', 'revenue', or 'expenses' to select the type\nof account that you would like to input.\n")
     print("If at any point you want to delete an account, finish entering all the information to the account you are currently entering.\nWhen asked for the next account, type 'del account name' but replace 'account name' with the account name.")
-    print('Note: You can only delete an account if that account is part of the menu you are in. For example, you can only delete assets when you are in the assets menu.\n')
+    print('Note: You can only delete an account if that account is part of the menu you are in. For example, you can only\ndelete assets when you are in the assets menu.\n')
     print("Press esc to go back to the main menu to enter another type of account.\n")
     print("When all data is entered, go back to the main menu and type 'create' to start creating your worksheets.\n")
     while True:
         if home:
-            n = input('Please enter the type of account that you would like to enter: ')
+            if not error:
+                n = input('Please enter the type of account that you would like to enter: ')
+            else:
+                n = 'create'
+                error = False
             if n == 'assets':
                 assets = True
                 home = False
@@ -135,6 +149,18 @@ def entry():
                 expenses = True
                 home = False
             elif n == 'create':
+                name = input('Please enter the name of your company: ')
+                date = input('Please enter the date in the following format (Month day, year) - NO SHORT FORM: ')
+                f_period = input("Please enter 'year' or 'month' for the fiscal period: ")
+                if f_period == 'year':
+                    try:
+                        f_period_time = int(input("Please enter the year of the fiscal period: "))
+                    except ValueError:
+                        print('Not a valid year. Please re-enter information.')
+                        error = True
+                        continue
+                else:
+                    f_period_time = input('Please enter the month of the fiscal period: ')
                 entry_complete = True
                 break
             else:
@@ -162,6 +188,7 @@ def entry():
                     continue
                 if a_value < 0:
                     print('Value was invalid. Terminated previously entered asset. Please re-enter.')
+                    continue
                 a_cr_dr = input("Please enter 'dr' for a debit balance or 'cr' for a credit balance: ")
                 if a_cr_dr == 'dr' or a_cr_dr == 'cr':
                     pass
@@ -171,7 +198,8 @@ def entry():
                 asset_name.append(a_name)
                 asset_value.append(a_value)
                 asset_cr_dr.append(a_cr_dr)
-            print(f'Your current assets are {asset_name}')
+            if a_name != 'back':
+                print(f'Your current assets are {asset_name}')
         elif liabilities:
             l_name = input('Please enter the name of your liability: ')
             if 'del' in l_name:
@@ -193,6 +221,9 @@ def entry():
                 except ValueError:
                     print('Value was invalid. Terminated previously entered liability. Please re-enter.')
                     continue
+                if l_value < 0:
+                    print('Value was invalid. Terminated previously entered liability. Please re-enter.')
+                    continue
                 l_cr_dr = input("Please enter 'dr' for a debit balance or 'cr' for a credit balance: ")
                 if l_cr_dr == 'dr' or l_cr_dr == 'cr':
                     pass
@@ -202,11 +233,78 @@ def entry():
                 liability_name.append(l_name)
                 liability_value.append(l_value)
                 liability_cr_dr.append(l_cr_dr)
-            print(f'Your current liabilities are {liability_name}')
+            if l_name != 'back':    
+                print(f'Your current liabilities are {liability_name}')
         elif capital:
-            pass
+            c_name = input('Please enter the name of your capital account: ')
+            if 'del' in c_name:
+                delete = c_name.find('l')
+                try:
+                    index = capital_name.index(c_name[delete+2:])
+                except ValueError:
+                    print('Capital account not found. Please re-enter.')
+                    continue
+                capital_name.pop(index)
+                capital_value.pop(index)
+                capital_cr_dr.pop(index)
+            elif c_name == 'back':
+                home = True
+                capital = False
+            else:
+                try:
+                    c_value = float(input('Please enter the amount of capital for this account: '))
+                except ValueError:
+                    print('Value was invalid. Terminated previously entered capital account. Please re-enter.')
+                    continue
+                if c_value < 0:
+                    print('Value was invalid. Terminated previously entered capital account. Please re-enter.')
+                    continue
+                c_cr_dr = input("Please enter 'dr' for a debit balance or 'cr' for a credit balance: ")
+                if c_cr_dr == 'dr' or c_cr_dr == 'cr':
+                    pass
+                else:
+                    print('Input was invalid. Terminated previously entered capital account. Please re-enter.')
+                    continue
+                capital_name.append(c_name)
+                capital_value.append(c_value)
+                capital_cr_dr.append(c_cr_dr)
+            if c_name != 'back':
+                print(f'Your current capital accounts are {capital_name}')
         elif drawings:
-            pass
+            d_name = input('Please enter the name of your drawings account: ')
+            if 'del' in d_name:
+                delete = d_name.find('l')
+                try:
+                    index = drawing_name.index(d_name[delete+2:])
+                except ValueError:
+                    print('Drawings account not found. Please re-enter.')
+                    continue
+                drawing_name.pop(index)
+                drawing_value.pop(index)
+                drawing_cr_dr.pop(index)
+            elif d_name == 'back':
+                home = True
+                drawings = False
+            else:
+                try:
+                    d_value = float(input('Please enter the amount of drawings for this account: '))
+                except ValueError:
+                    print('Value was invalid. Terminated previously entered drawings account. Please re-enter.')
+                    continue
+                if d_value < 0:
+                    print('Value was invalid. Terminated previously entered drawings account. Please re-enter.')
+                    continue
+                d_cr_dr = input("Please enter 'dr' for a debit balance or 'cr' for a credit balance: ")
+                if d_cr_dr == 'dr' or d_cr_dr == 'cr':
+                    pass
+                else:
+                    print('Input was invalid. Terminated previously entered drawings account. Please re-enter.')
+                    continue
+                drawing_name.append(d_name)
+                drawing_value.append(d_value)
+                drawing_cr_dr.append(d_cr_dr)
+            if d_name != 'back':
+                print(f'Your current drawings accounts are {drawing_name}')
         elif revenue:
             r_name = input('Please enter the name of your revenue: ')
             if 'del' in r_name:
@@ -228,35 +326,7 @@ def entry():
                 except ValueError:
                     print('Value was invalid. Terminated previously entered revenue account. Please re-enter.')
                     continue
-                r_cr_dr = input("Please enter 'dr' for a debit balance or 'cr' for a credit balance: ")
-                if r_cr_dr == 'dr' or r_cr_dr == 'cr':
-                    pass
-                else:
-                    print('Input was invalid. Terminated previously entered revenue account. Please re-enter.')
-                    continue
-                revenue_name.append(r_name)
-                revenue_value.append(r_value)
-                revenue_cr_dr.append(r_cr_dr)
-            print(f'Your current revenue accounts are {revenue_name}')
-        elif expenses:
-            r_name = input('Please enter the name of your revenue: ')
-            if 'del' in r_name:
-                delete = r_name.find('l')
-                try:
-                    index = revenue_name.index(r_name[delete+2:])
-                except ValueError:
-                    print('Revenue account not found. Please re-enter.')
-                    continue
-                revenue_name.pop(index)
-                revenue_value.pop(index)
-                revenue_cr_dr.pop(index)
-            elif r_name == 'back':
-                home = True
-                revenue = False
-            else:
-                try:
-                    r_value = float(input('Please enter the amount of revenue for this account: '))
-                except ValueError:
+                if r_value < 0:
                     print('Value was invalid. Terminated previously entered revenue account. Please re-enter.')
                     continue
                 r_cr_dr = input("Please enter 'dr' for a debit balance or 'cr' for a credit balance: ")
@@ -268,7 +338,43 @@ def entry():
                 revenue_name.append(r_name)
                 revenue_value.append(r_value)
                 revenue_cr_dr.append(r_cr_dr)
-            print(f'Your current revenue accounts are {revenue_name}')
+            if r_name != 'back':
+                print(f'Your current revenue accounts are {revenue_name}')
+        elif expenses:
+            e_name = input('Please enter the name of your expense: ')
+            if 'del' in e_name:
+                delete = e_name.find('l')
+                try:
+                    index = expense_name.index(e_name[delete+2:])
+                except ValueError:
+                    print('Expense not found. Please re-enter.')
+                    continue
+                expense_name.pop(index)
+                expense_value.pop(index)
+                expense_cr_dr.pop(index)
+            elif e_name == 'back':
+                home = True
+                expenses = False
+            else:
+                try:
+                    e_value = float(input('Please enter the cost of the expense: '))
+                except ValueError:
+                    print('Value was invalid. Terminated previously entered expense. Please re-enter.')
+                    continue
+                if e_value < 0:
+                    print('Value was invalid. Terminated previously entered expense. Please re-enter.')
+                    continue
+                e_cr_dr = input("Please enter 'dr' for a debit balance or 'cr' for a credit balance: ")
+                if e_cr_dr == 'dr' or e_cr_dr == 'cr':
+                    pass
+                else:
+                    print('Input was invalid. Terminated previously entered expense. Please re-enter.')
+                    continue
+                expense_name.append(e_name)
+                expense_value.append(e_value)
+                expense_cr_dr.append(e_cr_dr)
+            if e_name != 'back':
+                print(f'Your current expenses are {expense_name}')
 
 
 if __name__ == '__main__':
